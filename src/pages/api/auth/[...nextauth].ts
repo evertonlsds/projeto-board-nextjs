10779b2b2ab17cf0439178091b18dc2dddea0d3f
Everton santos <everton0102@gmail.com>
2022-03-04 14:19:28 -0300

    verificação de apoiadores

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,38 +2,55 @@ import { profile } from "console";
 import NextAuth from "next-auth";
 import { session } from "next-auth/client";
 import Providers from "next-auth/providers";
+import firebase from "../../../services/firebaseConnection";
 
 export default NextAuth({
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      scope:'read:user'
+      scope: "read:user",
     }),
   ],
-  callbacks:{
-      async session(session, profile){
-        try{
-            return{
-             ...session,
-             id: profile.sub  
+  callbacks: {
+    async session(session, profile) {
+      try {
+        const lastDonate = await firebase
+          .firestore()
+          .collection("users")
+          .doc(String(profile.sub))
+          .get()
+          .then((snapshot) => {
+            if (snapshot.exists) {
+              return snapshot.data().lastDonate.toDate();
+            } else {
+              return null; //que esse user não é apoiador
             }
-        }catch{
-           return{
-               ...session,
-               id:null
-           } 
-        }
-      },
-      async signIn(user, account, profile){
-        const { email } = user;
-        try{
-                return true;
-        }catch(err){
-            console.log('Deu erro', err);
-            return false;
-        }
+          });
 
+        return {
+          ...session,
+          id: profile.sub,
+          vip: lastDonate ? true : false,
+          lastDonate: lastDonate
+        };
+      } catch {
+        return {
+          ...session,
+          id: null,
+          vip: false,
+          lastDonate: null
+        };
       }
-  }
+    },
+    async signIn(user, account, profile) {
+      const { email } = user;
+      try {
+        return true;
+      } catch (err) {
+        console.log("Deu erro", err);
+        return false;
+      }
+    },
+  },
 });
